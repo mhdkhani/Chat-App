cfddd275da795642686c7e8101bc4d508f7358ed
root <root@localhost.localdomain>
2021-04-29 23:02:30 +0430

    custom broadcating event

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+let socket = io();
+socket.on('connect',function(){
+    console.log('Connected to Server.');
+})
+socket.on('disconnect',function(){
+    console.log('Disconnect from Server.')
+});
+socket.on('newMessage' , function (message) {
+console.log('new message: ' , message);
+})
--- server/server.js
+++ server/server.js
@@ -9,6 +9,29 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('A new user just connected.');
+    socket.emit('newMessage' , {
+        from: 'Admin' ,
+        text: 'Welcome to Chat Application.',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage' , {
+        from: 'Admin' ,
+        text: 'New User Joined.',
+        createdAt: new Date().getTime()
+    });
+    socket.on('createMessage' , (message) => {
+        console.log("message" , message);
+         io.emit('newMessage' , {
+            from: message.from ,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        /*socket.broadcast.emit('newMessage' , {
+            from: message.from ,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });*/
+    })
     socket.on('disconnect', () => {
         console.log('User Was Disconnected.');
     })
